15dce5efde8d42b3dbeb65b1525ac4da27a2e0c1
Kyle Just <kjjust7@gmail.com>
2024-02-26 19:10:06 -0800

    Added spotify_handler.py

--- backend/src/secrets_handler.py
+++ backend/src/secrets_handler.py
@@ -45,7 +45,7 @@ class SecretsHandler:
         return SecretsHandler._get_json_value("open-ai", "prompt")
 
     @staticmethod
-    def get_spotify_key() -> str:
+    def get_spotify_client_id() -> str:
         """
         Retrieves the Spotify API key using key spotify and sub-key api-key
 
@@ -54,4 +54,16 @@ class SecretsHandler:
         str
             The Spotify API key
         """
-        return SecretsHandler._get_json_value("spotify", "api-key")
+        return SecretsHandler._get_json_value("spotify", "client_id")
+
+    @staticmethod
+    def get_spotify_client_secret() -> str:
+        """
+        Retrieves the Spotify API key using key spotify and sub-key api-key
+
+        Returns
+        -------
+        str
+            The Spotify API key
+        """
+        return SecretsHandler._get_json_value("spotify", "client_secret")
