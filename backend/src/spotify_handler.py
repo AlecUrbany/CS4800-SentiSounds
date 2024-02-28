e41a050011422ae5a033aca545e29334afb21e8c
meap02 <79549780+meap02@users.noreply.github.com>
2024-02-27 21:18:36 -0800

    Remove static methods from SpotifyHandler

--- backend/src/spotify_handler.py
+++ backend/src/spotify_handler.py
@@ -6,7 +6,7 @@ import random
 
 class SpotifyHandler:
     """
-    A static class to handle all Spotify API interactions.
+    A class to handle all Spotify API interactions.
 
     Contains a definition for a static _client_instance. This should only be
     accessed via the `get_client()` function, which will automatically fill
@@ -16,16 +16,9 @@ class SpotifyHandler:
     """
 
     def __init__(self) -> None:
-        raise TypeError(
-            "SpotifyHandler instances should not be created. " +
-            "Consider using the `get_client()` function."
-        )
+        self._client_instance = None
 
-    # A static reference to the Spotify OAuth client
-    _client_instance: Spotify | None = None
-
-    @staticmethod
-    def get_base_client() -> Spotify:
+    def get_base_client(self) -> Spotify:
         """
         Retrieves or creates the Spotify client instance with no user credentials
 
@@ -34,13 +27,12 @@ class SpotifyHandler:
         Spotify
             The static Spotify client
         """
-        if SpotifyHandler._client_instance:
-            return SpotifyHandler._client_instance
+        if self._client_instance:
+            return self._client_instance
 
-        return SpotifyHandler._initialize_client()
+        return self._initialize_base_client()
 
-    @staticmethod
-    def _initialize_base_client() -> Spotify:
+    def _initialize_base_client(self) -> Spotify:
         """
         Initializes the Spotify client with no user credentials.
 
@@ -52,16 +44,15 @@ class SpotifyHandler:
         Spotify
             The static Spotify client
         """
-        SpotifyHandler._client_instance = Spotify(
+        self._client_instance = Spotify(
             client_credentials_manager=SpotifyClientCredentials(
                 client_id=SecretsHandler.get_spotify_client_id(),
                 client_secret=SecretsHandler.get_spotify_client_secret()
             )
         )
-        return SpotifyHandler._client_instance
+        return self._client_instance
 
-    @staticmethod
-    def get_user_client() -> Spotify:
+    def get_user_client(self) -> Spotify:
         """
         Retrieves or creates the Spotify client instance with a specific user credentials
 
@@ -70,13 +61,12 @@ class SpotifyHandler:
         Spotify
             The static Spotify client
         """
-        if SpotifyHandler._client_instance:
-            return SpotifyHandler._client_instance
+        if self._client_instance:
+            return self._client_instance
 
-        return SpotifyHandler._initialize_user_client()
+        return self._initialize_user_client()
 
-    @staticmethod
-    def _initialize_user_client() -> Spotify:
+    def _initialize_user_client(self) -> Spotify:
         """
         Initializes the Spotify client with user credentials.
 
@@ -90,7 +80,7 @@ class SpotifyHandler:
         """
         redirect_uri = "https://github.com/AlecUrbany/CS4800-SentiSounds"
         scope = ["streaming", "playlist-modify-private", "user-top-read", "user-read-private"]
-        SpotifyHandler._client_instance = Spotify(
+        self._client_instance = Spotify(
             auth_manager=SpotifyOAuth(
                 scope=scope,
                 redirect_uri=redirect_uri,
@@ -99,12 +89,30 @@ class SpotifyHandler:
                 client_secret=SecretsHandler.get_spotify_client_secret()
             )
         )
-        return SpotifyHandler._client_instance
+        return self._client_instance
 
-    @staticmethod
-    def get_genre_songs(genre: str, market: str, limit: int=10) -> list[dict]:
+    
+    def get_genre_songs(self, genre: str, market: str, limit: int=10) -> list[dict]:
         """
         Retrieves a pseudo-random list of songs in a genre sourced from the Spotify API.
+
+        Possible genres:
+        ['acoustic', 'afrobeat', 'alt-rock', 'alternative', 'ambient', 'anime', 'black-metal',
+        'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 
+        'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 
+        'deep-house', 'detroit-techno', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 
+        'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 
+        'groove', 'grunge', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop',
+        'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-dance', 
+        'j-idol', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 
+        'metal-misc', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'new-release', 'opera', 'pagode',
+        'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 
+        'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 
+        'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 
+        'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 
+        'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
+
+        This list may be user specific, so use the get_available_genre_seeds to find user genres
 
         Returns
         -------
@@ -119,5 +127,16 @@ class SpotifyHandler:
         """
         random_offset = random.randint(0, 1000)
         keys_to_extract = ["name", "preview_url", "uri", "explicit", "is_playable", "popularity"]
-        tracks_all = SpotifyHandler._client_instance.search(q='genre:' + genre, type="track", market=market, offset=random_offset, limit=limit)["tracks"]["items"]
+        tracks_all = self._client_instance.search(q='genre:' + genre, type="track", market=market, offset=random_offset, limit=limit)["tracks"]["items"]
         return [{key: track[key] for key in keys_to_extract} for track in tracks_all]
+    
+    def get_available_genre_seeds(self) -> list[str]:
+        """
+        Retrieves a list of available genre seeds from the Spotify API.
+
+        Returns
+        -------
+        list[str]
+            The list of available genre seeds
+        """
+        return self._client_instance.recommendation_genre_seeds()["genres"]
