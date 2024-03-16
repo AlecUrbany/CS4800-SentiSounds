979ac73f68b1f8ebce1d6290b5f09702c762f249
GEORGE MATTA <jojomedhat2004@gmail.com>
2024-03-15 21:08:06 -0700

    Simplify main to quickly run app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,10 @@
 """The main driver for beginning the program's execution"""
 
 from api import app
-from spotify_handler import SpotifyHandler
+# from spotify_handler import SpotifyHandler
 
 if __name__ == "__main__":
-    s = SpotifyHandler()
-    s.get_user_client()
-    s.get_genre_songs("rock")
+    # s = SpotifyHandler()
+    # s.get_user_client()
+    # s.get_genre_songs("rock")
     app.run()
