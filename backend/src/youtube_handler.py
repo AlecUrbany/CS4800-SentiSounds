6d5ff1e6549920b24cc368f07c0b743e2bcc1f68
Kyle Just <kjjust7@gmail.com>
2024-04-16 17:38:27 -0700

    added Youtube API search and multithreading capability

--- backend/src/youtube_handler.py
+++ backend/src/youtube_handler.py
@@ -18,8 +18,11 @@ class YoutubeHandler:
     """
     api_service_name = "youtube"
     api_version = "v3"
-    MAX_THREAD_COUNT = 5
+    MAX_THREAD_COUNT: int = 5
     _youtube_instance: discovery.Resource | None = None
+    request_queue: queue.Queue = queue.Queue()
+    thread_pool: list[threading.Thread] = []
+
 
     def __init__():
         """
@@ -55,3 +58,72 @@ class YoutubeHandler:
             developerKey=SecretsHandler.get_youtube_key()
         )
         return _youtube_instance
+    
+    @staticmethod
+    def search_for_match(song: dict):
+        """
+        Searches for a matching song on youtube given the song and artist name
+        Adds the youtube url to the song dictionary
+        if no match is found, the song is returned as None
+
+        Returns
+        -------
+        googleapiclient.discovery.Resource
+            The search results
+        """
+        youtube_url = "https://www.youtube.com/watch?v=%s" 
+
+        response = YoutubeHandler.get_client().search().list(
+            part = "id",
+            q = song["name"] + " " + song["artists"][0]["name"],
+            type = "video",
+            maxResults = 1,
+            order = "relevance",
+            fields = "items(id(videoId))"
+        ).execute()
+        if response["items"]:
+            song["youtube_url"] = youtube_url % response["items"][0]["id"]["videoId"]
+        else:
+            song["youtube_url"] = None
+        return song 
+    
+    @classmethod
+    def match_list(cls, songs: list[dict[str, str | bool | int]]):
+        """
+        Matches a list of songs to their respective youtube videos
+
+        Parameters
+        ----------
+        songs : list[dict[str, str | bool | int]]
+            A list of songs to match
+        """
+        queue
+        for song in songs:
+            cls.request_queue.put(song)
+        for _ in range(cls.MAX_THREAD_COUNT):
+            thread = threading.Thread(target=cls._process_queue)
+            thread.start()
+            cls.thread_pool.append(thread)
+
+        cls.request_queue.join()
+
+        for _ in range(cls.MAX_THREAD_COUNT): 
+            cls.request_queue.put(None) # Add a None to the queue for each thread to stop processing
+
+
+    @classmethod
+    def _process_queue(cls):
+        """
+        Processes the request queue
+        """
+        while True:
+            # Get a request from the queue
+            song = cls.request_queue.get()
+            
+            # If the queue is empty, break the loop
+            if song is None:
+                break
+            
+            cls.search_for_match(song)
+            # Mark the task as done
+            cls.request_queue.task_done()
