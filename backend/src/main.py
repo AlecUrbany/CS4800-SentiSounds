7a644b715776fb47f69e1e6a43645a235a0a0541
GEORGE MATTA <jojomedhat2004@gmail.com>
2024-04-30 17:26:39 -0700

    Bind hypercorn port

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,13 @@
 """The main driver for beginning the program's execution"""
 
+import asyncio
+
 from api import app
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 
 if __name__ == "__main__":
-    app.run()
+    config = Config()
+    config.bind = ["172.208.98.187:5000"]
+
+    asyncio.run(serve(app, config))
