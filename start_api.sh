3636714eff065d15c315c93163f73130e6b2d4d5
Ubuntu <sentisounds@SentiAPI.jhuf023wcdkupo2snqf0g3vg3f.bx.internal.cloudapp.net>
2024-05-03 07:25:41 +0000

    Created script to automatically start the SentiSounds API

---
+++ start_api.sh
@@ -0,0 +1,15 @@
+# Check if PSQL dtabase is running
+PSQL_STATUS="$(systemctl is-active postgresql.service)"
+if [ "${PSQL_STATUS}" = "active" ]; then
+	echo 'PSQL service is running...'
+else
+	echo 'Starting PSQL service'
+	systemctl start postgresql
+fi
+
+# Ensure pip requirements are up to date
+echo 'Ensuring python packages are up to date...'
+pip install -r requirements.txt > .start_api_pip.log
+
+echo 'Starting SentiSounds API...' 
+python3 backend/src/main.py
