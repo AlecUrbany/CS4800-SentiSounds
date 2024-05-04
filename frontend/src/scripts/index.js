0b8d00b6737bee25c059595ff08b90990783ba7a
meap02 <79549780+meap02@users.noreply.github.com>
2024-05-03 19:20:41 -0700

    Spotify post-authentication process adjustments

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -20,6 +20,8 @@
                     isAuthenticated = data.is_authenticated;
                     if (isAuthenticated) {
                       document.getElementById("exportPlaylistBtn").hidden = false;
+                      document.getElementById("connectSpotifyBtn").hidden = true;
+                      document.getElementById("spotifyImg").hidden = true;
                     } else {
                       document.getElementById("exportPlaylistBtn").hidden = true;
                       document.getElementById("connectSpotifyBtn").hidden = false;
@@ -33,7 +35,7 @@
       }
 
       document
-        .getElementById("searchForm")
+        .getElementById("searchForm") 
         .addEventListener("submit", function (event) {
           event.preventDefault();
           const enteredPrompt = document.getElementById("default-search").value;
@@ -240,7 +242,7 @@
       const code = getURLParameter("code");
 
       // You can now use 'code' variable throughout your script
-      console.log("Code extracted from URL:", code);
+      //console.log("Code extracted from URL:", code);
 
       if (code.length > 5) {
         document.addEventListener("DOMContentLoaded", async function () {
@@ -261,7 +263,7 @@
 
             if (response.ok) {
               alert("Spotify Authentication successful.");
-              // window.location.href = "index.html";
+              window.location.href = "index.html";
             } else {
               alert(data.error);
             }
