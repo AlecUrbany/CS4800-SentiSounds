7cae4d4881ced7b702aa82d53b58daf62c3e1723
Ubuntu <sentisounds@SentiAPI.jhuf023wcdkupo2snqf0g3vg3f.bx.internal.cloudapp.net>
2024-05-05 01:48:17 +0000

    Added some management scripts to update packages and enable services

---
+++ scripts/enable_services.sh
@@ -0,0 +1,4 @@
+sudo systemctl enable --now /home/sentisounds/CS4800-SentiSounds/services/sentisoundsAPI.service
+sudo apt install nginx
+sudo ln -sf services/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo systemctl enable --now nginx.service
